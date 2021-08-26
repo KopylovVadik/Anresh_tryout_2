3c16c1d2a4a3078bbbd86d0d6082db8d59fb0a67
KopylovVadik <kopylov.ddut@gmail.com>
2021-08-26 17:31:41 +0300

    anresh task 2

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            string path = @"C:\Users\kopyl\Desktop\Новый текстовый документ.txt";
+            string[] text = System.IO.File.ReadAllLines(path);
+            return Ok(text);
+        }
+
+        [HttpPut]
+        public ActionResult Update([FromBody] JsonElement body)
+        {
+            string text = body.GetProperty("value").ToString();
+            string path = @"C:\Users\kopyl\Desktop\Новый текстовый документ.txt";
+            System.IO.File.WriteAllText(path, text);
+            return Ok(text);
+
+        }
+
+    }
+}
+
+
+
---
+++ WebApplication1/wwwroot/js/JavaScript.js
@@ -0,0 +1,84 @@
+﻿
+// Получение данных из файла
+async function GetData() {
+    // отправляет запрос и получаем ответ
+    const response = await fetch("/api/values", {
+        method: "GET",
+        headers: { "Accept": "application/json" }
+    });
+
+    // если запрос прошел нормально
+    if (response.ok === true) {
+        // получаем данные
+        const data = await response.json();
+        //console.log(data)
+        const filtered_data = data.filter((item) => {
+            return item !== '';
+        });
+        //console.log(filtered_data);
+        document.querySelector('#w3review').value = filtered_data;
+    }
+
+}
+
+
+// Редактирование данных из файла
+async function EditData(value) {
+    const response = await fetch("api/values", {
+        method: "PUT",
+        headers: { "Accept": "application/json", "Content-Type": "application/json" },
+        body: JSON.stringify({
+            value
+        })
+    });
+    if (response.ok === true) {
+        const result = await response.json();
+        //console.log(result);
+       
+        //reset();
+    }
+}
+
+
+// Обновление данных из файла
+async function UpdateData() {
+    // отправляет запрос и получаем ответ
+    const response = await fetch("/api/values", {
+        method: "GET",
+        headers: { "Accept": "application/json" }
+    });
+
+    // если запрос прошел нормально
+    if (response.ok === true) {
+        // получаем данные
+        const data = await response.json();
+        
+        document.querySelector('#w3review').value = data;
+
+    }
+
+}
+
+
+let save_button = document.querySelector('#submit')
+save_button.addEventListener('click', (e) => {
+    e.preventDefault()
+    const value = document.querySelector('#w3review').value
+    // логика обработки отправки
+    //console.log(value.split(","))
+    EditData(value);
+})
+
+
+
+
+let update_button = document.querySelector('#reset')
+update_button.addEventListener('click', (e) => {
+    e.preventDefault()
+   
+    UpdateData();
+})
+
+
+// вызов функции
+GetData();
